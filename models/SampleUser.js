0d5356172969bb043a34c8e76da960c62d064c28
TJ <evertang@gmail.com>
2014-01-17 23:13:55 -0500

    clean up client

--- models/SampleUser.js
+++ models/SampleUser.js
@@ -1,4 +1,5 @@
-var mongoose = require('mongoose');
+var angoose = require("../lib/angoose");
+var mongoose = angoose.getMongoose();
 var SampleSchema = mongoose.Schema({
         email:  {type: String, required: true, match: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, unique:true},
         firstname: {type: String, required:true },
