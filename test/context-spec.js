38a8bf555468d8f78b0dbbb59356129f048bf619
TJ <evertang@gmail.com>
2014-01-29 12:17:58 -0500

    remove local-storage

--- test/context-spec.js
+++ test/context-spec.js
@@ -7,8 +7,6 @@ var request = require('request');
 var logging = require("log4js");
 var async = require("../lib/util/toolbox").async;
   
-var localStorage = require('continuation-local-storage');
-var session = localStorage.createNamespace('Session');
 
 
 var util = require("./test-util");
@@ -28,38 +26,8 @@ var MyError = function(message, value){
 
 var donedone = false;
 describe("Angoose Local Storage Tests", function(){
-    xit("Test context", function(done){
-        // no longer use local-storage        
-        for(var i=0;i<5;i++){
-            console.log("setting up "+i)
-            session.run(function(){
-                setUp( i+1, doWork);    
-            })
-        }
-        setTimeout(function(){
-            if(donedone)
-                console.log("Good");
-            else
-                expect("Incomplete").toBe("Completed");
-            done();
-        }, 15)
-    });
-     xit("Test async context lost", function(done){
-        
-        var fn = function(){
-            var s = localStorage.getNamespace("Session2")
-            expect(s.get("val")).toBe(1);
-            console.log("async func test called, val should be 1, got", s.get("val"));
-            done();
-        }
-        var sess = localStorage.createNamespace('Session2');
-        sess.run(function(){
-            sess.set("val", 1);
-            var f = async(async(async(async(async(async(fn))))));
-            async(async(async(f)))();
-        })
-    });
-     
+   
+      
     it("Execution Context", function(done){
         console.log("Execution context test");
         eval(clientSource);
@@ -75,19 +43,5 @@ describe("Angoose Local Storage Tests", function(){
    
     
     
-}); 
-function setUp(val, func){
-    session.set("count", val );
-    process.nextTick(func)
-}
-
-function doWork(){
-    var session = localStorage.getNamespace('Session');            
-    console.log("COUNT", session.get("count"));
-    var count = session.get("count");
-    if(count>=4){
-        console.log("DONE!");
-        donedone = true;
-    }             
-}
+});  
 
